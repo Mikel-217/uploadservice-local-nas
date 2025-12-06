3a67343a8c00d5942a407320e0226fcc4171ed1e
Mikel <mikel.kunze887@web.de>
2025-12-06 15:14:50 +0100

    Added sql-statements | Changes to dbstruct

---
+++ Database/SQL-commands/create-table.sql
@@ -0,0 +1,27 @@
+CREATE TABLE Users (
+    UserID int NOT NULL AUTO_INCREMENT,
+    UserName varchar(255),
+    UserPW varchar(255),
+
+    PRIMARY KEY (ID)
+);
+
+
+CREATE TABLE UserDirectorys (
+    DirID int NOT NULL AUTO_INCREMENT,
+    UserID int NOT NULL,
+    DirName varchar(255),
+    DirPath varchar(255),
+
+    PRIMARY KEY (DirID),
+    FOREIGN KEY (UserID) REFERENCES Users(UserID)
+);
+
+
+CREATE TABLE ActiveAccessTokens (
+    TokenID int NOT NULL,
+    ActiveToken varchar(255),
+    ExpirationDate TIMESTAMP,
+
+    PRIMARY KEY (TokenID)
+);
--- Database/dbstructs.go
+++ Database/dbstructs.go
@@ -3,18 +3,20 @@ package database
 import "time"
 
 type UserStruct struct {
-	ID       uint   // Is also inclided in the JWT
+	ID       uint   // Is also included in the JWT
 	UserName string // Is also included in the JWT
 	PW       string // !important: always hash this string
-	Dirs     UserDirectorys
 }
 
 type UserDirectorys struct {
+	DirID   uint
+	UserID  uint // foreign key for User
 	DirName string
 	DirPath string
 }
 
 type ActiveAccessTokens struct {
+	TokenID        uint
 	ActiveToken    string
 	ExpirationDate time.Time
 }
