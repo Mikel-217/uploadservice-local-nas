f233bdb37d85e892e668f9c330b6f42523d9f2b1
Mikel <mikel.kunze887@web.de>
2025-12-06 17:25:25 +0100

    Renamed "createdbcon" | Added database functions

--- Database/database.go
+++ Database/database.go
@@ -1,13 +1,72 @@
 package database
 
-import "time"
+import (
+	"time"
 
-// TODO: implement Database functions
-// - func for storring accesstokens
-// - func for storring userdata
+	logging "mikel-kunze.com/uploadservice/Logging"
+)
 
-func SetNewToken(token string, expiration time.Time) {}
+// Sets a new Active token to DB
+func SetNewToken(token string, expiration time.Time) {
+	db := CreateDBCon()
 
-func GetUserByName(userName string) UserStruct { return UserStruct{} }
+	if db == nil {
+		logging.LogEntry("[Error]", "Cannot connect to db!")
+	}
 
-func CheckTokenExistence(token string) bool { return false }
+	defer db.Close()
+
+	if _, err := db.Exec("INSERT INTO ActiveAccessTokens (TokenID, ActiveToken, ExpirationDate) VALUES (DEFAULT, ?, ?)", token, expiration); err != nil {
+		logging.LogEntry("[Error]", err.Error())
+	}
+}
+
+// Gets a user by the given username --> Returns a Userstruct
+func GetUserByName(userName string) UserStruct {
+	db := CreateDBCon()
+
+	if db == nil {
+		logging.LogEntry("[Error]", "Cannot connect to db!")
+	}
+
+	defer db.Close()
+
+	var user UserStruct
+
+	err := db.QueryRow("SELECT * FROM Users WHERE UserName = ?", userName).Scan(&user.ID, &user.UserName, &user.UserName)
+
+	if err != nil {
+		logging.LogEntry("[Error]", err.Error())
+	}
+
+	return user
+}
+
+// Gets all UserDirs by the given userID --> Returns a slice
+func GetUserDirs(userID uint) []UserDirectorys {
+	return []UserDirectorys{}
+}
+
+// Checks if the token is saved in the DB
+func CheckTokenExistence(token string) bool {
+	db := CreateDBCon()
+
+	if db == nil {
+		logging.LogEntry("[Error]", "Cannot connect to db!")
+		return false
+	}
+
+	defer db.Close()
+
+	var tokenDB ActiveAccessTokens
+
+	if err := db.QueryRow("SELECT * FROM ActiveAccessTokens WHERE ActiveToken = ?", token).Scan(&tokenDB.TokenID, &tokenDB.ExpirationDate, &tokenDB.ActiveToken); err != nil {
+		return false
+	}
+
+	if tokenDB.ActiveToken == token && tokenDB.ExpirationDate != time.Now() {
+		return true
+	}
+
+	return true
+}
